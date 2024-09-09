920bafed04e30bade70dd881f87d5b5b3afe5091
swapnilsparsh <arezona.lucky55@gmail.com>
2024-09-09 14:44:02 +0530

    Google Analytics added

--- src/main.js
+++ src/main.js
@@ -2,5 +2,20 @@ import { createApp } from 'vue'
 import './tailwind.css'
 import App from './App.vue'
 
-// import './assets/main.css'
+function loadGoogleAnalytics() {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=G-V5G3W2P568`;
+  document.head.appendChild(script);
+
+  script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { window.dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-V5G3W2P568');
+  };
+}
+
+loadGoogleAnalytics();
+
 createApp(App).mount('#app')
